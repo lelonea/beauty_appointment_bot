627ce5ec60d7840a1caa296255b18e009ddea174
Anastasiya Lazar <leloneya@gmail.com>
2023-07-03 08:33:45 +0300

    models and migrations editing

--- src/db/models.py
+++ src/db/models.py
@@ -1,6 +1,5 @@
 from typing import List
-from typing import Optional
-from sqlalchemy import ForeignKey, Date, Time
+from sqlalchemy import ForeignKey, Date, Time, BigInteger
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
@@ -9,31 +8,33 @@ from sqlalchemy.orm import relationship
 
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(primary_key=True)
 
 
-class Master(Base):
+class AbstractUser(Base):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    username: Mapped[str] = mapped_column(String(64), nullable=True)  # user can exist without username
+    full_name: Mapped[str] = mapped_column(String(128))
+
+
+class Master(AbstractUser):
     __tablename__ = 'masters'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(50))
-    full_name: Mapped[str] = mapped_column(String(50))
     appointments: Mapped[List["Appointment"]] = relationship(back_populates="master")
+    available_dates: Mapped[List["AvailableSlot"]] = relationship("AvailableSlot", back_populates="master")
 
 
-class Client(Base):
+class Client(AbstractUser):
     __tablename__ = 'clients'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(50))
-    full_name: Mapped[str] = mapped_column(String(50))
     appointments: Mapped[List["Appointment"]] = relationship(back_populates="client")
 
 
 class AvailableSlot(Base):
     __tablename__ = 'available_slots'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     master_id: Mapped[int] = mapped_column(ForeignKey('masters.id'))
     date: Mapped[Date] = mapped_column(Date)
     time: Mapped[Time] = mapped_column(Time)
@@ -43,7 +44,6 @@ class AvailableSlot(Base):
 class Appointment(Base):
     __tablename__ = 'appointments'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     master_id: Mapped[int] = mapped_column(ForeignKey('masters.id'))
     client_id: Mapped[int] = mapped_column(ForeignKey('clients.id'))
     date: Mapped[Date] = mapped_column(Date)
